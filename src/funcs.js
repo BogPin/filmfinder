b8e26c705e2463447cb230d1ed304a7c52d57841
BogPin <bogdanpin2002@gmail.com>
2022-12-24 20:59:34 +0200

    add memoiz func

--- src/funcs.js
+++ src/funcs.js
@@ -1,2 +1,17 @@
 const request = require('request');
 const defs = require('./definitions');
+
+const loger = (err) => console.log(err);
+
+const memoize = (fn) => {
+    const cache = Object.create(null);
+    return async (...args) => {
+        const key = args[0];
+        const val = cache[key];
+        if (val) return val;
+        const res = await fn(...args);
+        cache[key] = res;
+        return res;
+    };
+};
+
