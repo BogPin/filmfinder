129a46321598f8db655f93f2ba5609a345aff002
BogPin <bogdanpin2002@gmail.com>
2022-12-25 16:57:06 +0200

    add request func and link generators

--- src/funcs.js
+++ src/funcs.js
@@ -15,3 +15,35 @@ const memoize = (fn) => {
     };
 };
 
+const makeRequest = (options) =>
+    new Promise((resolve, reject) => {
+        request(null, options, (error, response) => {
+            if (error) reject(error);
+            else resolve(JSON.parse(response.body));
+        });
+    });
+
+const imdbLinkGenerator = (genre) => {
+    const preLink = defs.imdbPrelink;
+    const postLink = defs.imdbPostlink;
+    return preLink + genre + postLink;
+};
+
+const kinopoiskLinkGenerator = (genre, page) => {
+    const preLink = defs.kinopoiskPrelink;
+    const postLink = defs.kinopoiskPostlink;
+    return preLink + genre + postLink + page;
+};
+
+const kinopoiskFromImdbLinkGenerator = (id) => {
+    const preLink = defs.imdbToKinopoiskPrelink;
+    const postLink = defs.imdbToKinopoiskPostlink;
+    return preLink + id + postLink;
+};
+
+const kinopoiskKeyWordLinkGenerator = (keyWords) => {
+    const preLink = defs.kinopoiskKeyWordPrelink;
+    const postLink = defs.kinopoiskKeyWordPostlink;
+    return preLink + encodeURI(keyWords) + postLink;
+};
+
